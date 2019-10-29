a7e4a00f75748e1595ea8bc6b5514e970db75fea
Lea-Brml <l.bramoulle@hotmail.fr>
2019-10-29 15:36:01 +0100

    maj j2

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -4,4 +4,12 @@ class Gossip < ApplicationRecord
 	#mais qu'un gossip peut avoir plusieur tag
 	has_many :tag_gossips
 	has_many :tags, through: :tag_gossip
+
+	validates :title, presence: {message: "Bonjour! Veux-tu bien renseigner un titre pour ton Gossip STP ?"}
+	validates :title, uniqueness: true
+	validates :title, length: {minimum: 3, maximum: 20, message: "Veux-tu bien renseigner un contenu un chouya plus long stp."}
+
+	validates :content, presence: {message: "Bonjour! Veux-tu bien renseigner un titre pour ton Gossip STP ?"}
+	validates :content, length: {minimum: 20, message: "Veux-tu bien renseigner un contenu un chouya plus long stp."}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
 	#qu'un user peut envoyer plusieur message et recevoir plusieur message
 	has_many :sent_messages, foreign_key: 'sender_id', class_name: "MessagePrive"
 	has_many :received_messages, foreign_key: 'recipient_id', class_name: "MessagePrive"
+
+	validates :first_name, presence:  {message: "Bonjour! Veux-tu bien renseigner ton prénom STP ?"}
+
+	validates :last_name, presence:  {message: "Bonjour! Veux-tu bien renseigner ton nom STP ?"}
+
+	validates :description, presence:  {message: "Bonjour! Veux-tu bien renseigner une description STP ?"}
+	validates :description, length: {minimum: 20, message: "La description est trop courte."}
+
+	validates :email, presence:  {message: "Bonjour! Veux-tu bien renseigner ton email STP ?"}
+	validates :email, uniqueness: true
+	validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Le dormat n'est pas correct. Renseigne un fomat d'adresse mail valide stp." }
+
 end
